ec1eb49fd704e15879189a5e2d40bb84d36ccfc5
research <research@research-ThinkStation-S20.(none)>
2013-05-18 08:25:04 +0200

    commit new version

--- lib/WWW/Coursera.pm
+++ lib/WWW/Coursera.pm
@@ -15,11 +15,11 @@ WWW::Coursera - Downloading material (video, text, pdf ...) from Coursera.org on
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
